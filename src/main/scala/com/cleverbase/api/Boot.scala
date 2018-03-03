57783106628222b9b08fb11ef96e73415cd988d7
Fernando Romero <fernandojhoel@gmail.com>
2018-03-04 00:24:40 +0100

    mongodb persistence and real user authentication

--- src/main/scala/com/cleverbase/api/Boot.scala
+++ src/main/scala/com/cleverbase/api/Boot.scala
@@ -2,6 +2,9 @@ package com.cleverbase.api
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{ Success, Failure }
 
 import akka.actor.{ ActorRef, ActorSystem }
 import akka.http.scaladsl.Http
@@ -12,9 +15,13 @@ object Boot extends App with Routes {
   implicit val system: ActorSystem = ActorSystem("cleverbase")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  def persistenceService = new MemoryPersistenceService()
+  val persistenceService = new MongoPersistenceService("mongodb://localhost:27017")
 
-  Http().bindAndHandle(routes, "localhost", 8080)
-  println(s"Server online at http://localhost:8080/")
-  Await.result(system.whenTerminated, Duration.Inf)
+  persistenceService.start.onComplete {
+    case Success(_) =>
+      Http().bindAndHandle(routes, "localhost", 8080)
+      println(s"Server online at http://localhost:8080/")
+    case Failure(f) =>
+      println(f)
+  }
 }
--- src/main/scala/com/cleverbase/api/PersistenceService.scala
+++ src/main/scala/com/cleverbase/api/PersistenceService.scala
@@ -1,6 +1,15 @@
 package com.cleverbase.api
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import org.mongodb.scala._
+import org.mongodb.scala.model._
+import org.mongodb.scala.model.Filters._
+
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.bson.codecs.configuration.CodecRegistries.{ fromRegistries, fromProviders }
 
 trait PersistenceService {
 
@@ -13,5 +22,27 @@ class MemoryPersistenceService extends PersistenceService {
 
   def getUser(username: String): Future[Option[User]] = {
     Future.successful(users.find(_.username == username))
+  }
+}
+
+class MongoPersistenceService(uri: String) extends PersistenceService {
+
+  private val mongoClient = MongoClient(uri)
+  private val codec = fromRegistries(fromProviders(classOf[User]), DEFAULT_CODEC_REGISTRY)
+  private val database = mongoClient.getDatabase("cleverbase").withCodecRegistry(codec)
+  private val users: MongoCollection[User] = database.getCollection("users")
+
+  def start: Future[String] = {
+    for {
+      _ <- users.insertOne(User("fernando", "romero")).toFuture().recover { case _ => Nil }
+      _ <- users.insertOne(User("remco", "vanwijk")).toFuture().recover { case _ => Nil }
+      r <- users.createIndex(Indexes.ascending("username"), IndexOptions().unique(true)).toFuture()
+    } yield {
+      r
+    }
+  }
+
+  def getUser(username: String): Future[Option[User]] = {
+    users.find(equal("username", username)).toFuture().map(_.headOption)
   }
 }
